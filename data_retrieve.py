5f2e1155636464f14221838da98a82452a01f3f3
TriDang2025 <tridang_2025@depauw.edu>
2025-04-07 22:07:43 -0400

    Add data preprocessing and data retrieve

---
+++ data_retrieve.py
@@ -0,0 +1,56 @@
+import os
+import re
+from PIL import Image
+import numpy as np
+
+def get_images_by_folder_id(folder_id):
+    """
+    Given an image folder ID (e.g., '1.3.6.1.4.1...'),
+    finds all images in that folder matching the pattern '1-xxx.jpg'.
+
+    Parameters:
+        folder_id (str): Name of the image folder
+
+    Returns:
+        List of (filename, image array) tuples
+    """
+    # Build full image path from folder_id
+    image_file_path = os.path.join(
+        "kagglehub",
+        "datasets",
+        "awsaf49",
+        "cbis-ddsm-breast-cancer-image-dataset",
+        "versions",
+        "1",
+        "jpeg",
+        folder_id
+    )
+
+    pattern = re.compile(r"^1-\d{3}\.jpg$", re.IGNORECASE)
+    matched_images = []
+
+    for file in os.listdir(image_file_path):
+        if pattern.match(file):
+            full_path = os.path.join(image_file_path, file)
+            img = Image.open(full_path).convert("RGB")
+            matched_images.append((file, np.array(img)))
+
+    return matched_images
+
+def extract_folder_id(image_file_path: str) -> str:
+    """
+    Extracts the 3rd part from the image file path.
+
+    Example:
+    From 'Calc-Test.../UID1/UID2/000000.dcm' → returns 'UID2'
+
+    Parameters:
+        image_file_path (str): path from CSV
+
+    Returns:
+        str: the folder UID containing the JPEG images
+    """
+    parts = image_file_path.split('/')
+    if len(parts) >= 3:
+        return parts[2]
+    return None
